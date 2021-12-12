229f38072729bef518689468a35f83b9531d0898
Bindudevraj <66455983+Bindudevraj@users.noreply.github.com>
2021-12-12 18:45:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+async function getdata() {
+    var res = await fetch('https://meme-api.herokuapp.com/gimme');
+    var data = await res.json();
+
+    document.getElementById('img').src = data.preview[0];
+    document.getElementById('url').innerText = data.url;
+    console.log(data.url);
+}
